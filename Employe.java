6afdbae812ed505f506bb52fe387c8dc3bc9fab7
239x1a3213-ctrl <239x1a3213@gprec.ac.in>
2025-09-02 17:20:26 +0530

    Add files via upload

--- Employe.java
+++ Employe.java
@@ -11,7 +11,24 @@ public class Employe {
 					System.out.println(E1.empname);
 					System.out.println(E1.id);
 					System.out.println(E1.salary);
+					
+			Employe E2=new Employe();
+			E2.empname="sudha";
+			E2.id=101;
+			E2.salary=30000.00;
+			System.out.println("Employee Name :"+E2.empname);
+			System.out.println("Employee ID :"+E2.id);
+			System.out.println("Salary :"+E2.salary);
+			
+			Employe E3=new Employe();
+			E2.empname="raghu";
+			E2.id=102;
+			E2.salary=30000.00;
+			System.out.println("Employee Name :"+E2.empname);
+			System.out.println("Employee ID :"+E2.id);
+			System.out.println("Salary :"+E2.salary);
 		}
+	
 		
 }
 
