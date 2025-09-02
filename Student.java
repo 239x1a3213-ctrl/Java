b8d1d65cd3d572aaafab398b702b62deb0088313
239x1a3213-ctrl <239x1a3213@gprec.ac.in>
2025-09-02 16:22:18 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Student {
+	String StdName="pooji";
+	int Regno=101;
+	String branch="CSE";
+	public static void main(String[] args) {
+		Student S1=new Student();
+		System.out.println("Student name : "+S1.StdName);
+		System.out.println("Regester num : "+S1.Regno);
+		System.out.println("Student branch : "+S1.branch);
+		Student S2=new Student();
+		System.out.println("Student name : "+S2.StdName);
+		System.out.println("Regester num : "+S2.Regno);
+		System.out.println("Student branch : "+S2.branch);
+		
+		
+	}
+
+}
