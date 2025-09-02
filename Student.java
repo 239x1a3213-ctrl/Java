fbd7843ecbb9104dd4058337041e232ad18de640
239x1a3213-ctrl <239x1a3213@gprec.ac.in>
2025-09-02 16:43:35 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,20 +1,53 @@
-package day1;
-
 public class Student {
-	String StdName="pooji";
-	int Regno=101;
-	String branch="CSE";
+	String StdName;
+	int Regno;
+	String branch;
+	double CGPA;
+
 	public static void main(String[] args) {
-		Student S1=new Student();
-		System.out.println("Student name : "+S1.StdName);
-		System.out.println("Regester num : "+S1.Regno);
-		System.out.println("Student branch : "+S1.branch);
-		Student S2=new Student();
-		System.out.println("Student name : "+S2.StdName);
-		System.out.println("Regester num : "+S2.Regno);
-		System.out.println("Student branch : "+S2.branch);
+		// TODO Auto-generated method stub
+		Student S1 = new Student();
+		S1.StdName="pooji";
+		S1.Regno=101;
+		S1.branch="CSE";
+		S1.CGPA=98.8;
+		System.out.println("Student Name :" +S1.StdName);
+		System.out.println("regester num:"+S1.Regno);
+		System.out.println("Branch :"+S1.branch);
+		System.out.println("precentage :"+S1.CGPA);
+		
+		Student S2 = new Student();
+		S2.StdName="siri";
+		S2.Regno=102;
+		S2.branch="CSD";
+		S2.CGPA=89.8;
+		System.out.println("Student Name :" +S2.StdName);
+		System.out.println("regester num:"+S2.Regno);
+		System.out.println("Branch :"+S2.branch);
+		System.out.println("precentage :"+S2.CGPA);
+		
+		Student S3 = new Student();
+		S3.StdName="teju";
+		S3.Regno=103;
+		S3.branch="CSM";
+		S3.CGPA=79.8;
+		System.out.println("Student Name :" +S3.StdName);
+		System.out.println("regester num:"+S3.Regno);
+		System.out.println("Branch :"+S3.branch);
+		System.out.println("precentage :"+S3.CGPA);
+		
+		Student S4 = new Student();
+		S4.StdName="akhi";
+		S4.Regno=104;
+		S4.branch="CSBS";
+		S4.CGPA=88.98;
+		System.out.println("Student Name:"+S4.StdName);
+		System.out.println("regester num:"+S4.Regno);
+		System.out.println("Branch :"+S4.branch);
+		System.out.println("percentage :"+S4.branch);
 		
 		
+
 	}
 
 }
