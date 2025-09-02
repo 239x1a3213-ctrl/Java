5e0ca9d855f4a0ce4fedb0ed50a0596497cb306a
239x1a3213-ctrl <239x1a3213@gprec.ac.in>
2025-09-02 17:18:38 +0530

    Add files via upload

--- Mobile.java
+++ Mobile.java
@@ -1,9 +1,23 @@
 package day1;
 
 public class Mobile {
-
+	String mname;
+	int price;
+	String colour;
+	double capacity;
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		Mobile M=new Mobile();
+		M.mname="OPPO";
+		M.price=20000;
+		M.colour="black";
+		M.capacity=5.89;
+		System.out.println("Mobile Name : "+M.mname);
+		System.out.println("Mobile Price : "+M.price);
+		System.out.println("Colour : "+M.colour);
+		System.out.println("Battery Capacity"+M.capacity);
+		
 
 	}
 
